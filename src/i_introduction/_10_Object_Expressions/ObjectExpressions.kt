92e66e67baace2b4eb1a83d902319890e5ed219c
RaggaJungler <starfall7@tut.by>
2015-03-02 00:33:50 +0300

    Introduction complete

---
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -0,0 +1,47 @@
+package i_introduction._10_Object_Expressions
+
+import util.TODO
+import java.awt.event.MouseListener
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+
+fun objectLiteral() {
+
+    abstract class Foo {
+        abstract fun foo()
+    }
+
+    // Anonymous object extending a class
+    val foo: Foo = object : Foo() {
+        override fun foo() {
+            // ...
+        }
+    }
+
+    // Anonymous object extending a trait (interface)
+    runInANewThread(object : Runnable {
+        override fun run() {
+            // ...
+        }
+    })
+}
+
+fun runInANewThread(runnable: Runnable) = Thread(runnable).start()
+
+fun todoTask10() = TODO(
+    """
+        Task 10.
+        Add an object expression that extends MouseAdapter and counts the number of mouse clicks
+        as an argument to the function 'handleMouse()'.
+    """
+)
+
+fun task10(handleMouse: (MouseListener) -> Unit): Int {
+    var mouseClicks = 0
+    handleMouse(object : MouseAdapter(){
+        override fun mouseClicked(e :MouseEvent){
+            mouseClicks++
+        }
+    })
+    return mouseClicks
+}
---
+++ src/i_introduction/_9_Extensions_On_Collections/ExtensionsOnCollections.kt
@@ -0,0 +1,84 @@
+package i_introduction._9_Extensions_On_Collections
+
+import util.TODO
+
+data class User(val name: String, val lastName: String, val age: Int)
+
+fun operationsWithCollections() {
+    val users = listOf(
+            User("John", "Doe", 19),
+            User("Jane", "Doe", 22)
+    )
+
+    //iterate over collection:
+    for (user in users) {
+    }
+
+    val minors = users.filter { u -> u.age < 21 }
+    println("${minors.size()} user(s) under 21 years old")
+
+    val youngestOver20 = users filter { u -> u.age > 20 } minBy { u -> u.age }
+    println("Youngest over 20: $youngestOver20")
+}
+
+fun operationsWithMaps() {
+
+    val map = hashMapOf(1 to "one", 3 to "three", 42 to "forty two")
+
+    //iterate over map:
+    for ((key, value) in map) {
+    }
+
+    //convenient way to access elements:
+    map[43] = map[42] + " plus one"
+
+    //the details (how it works) you'll found in 'Conventions' task later
+
+    println(map)
+}
+
+fun main(args: Array<String>) {
+    operationsWithCollections()
+    operationsWithMaps()
+}
+
+fun todoTask9() = TODO(
+    """
+        Task 9.
+        The function should do the same as 'JavaCode9.doSomethingStrangeWithCollection'
+        Replace all invocations of 'todoTask9()' with the appropriate code.
+    """,
+    references = { (c: Collection<String>) -> JavaCode9().doSomethingStrangeWithCollection(c) }
+)
+
+fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+//    Map<Integer, List<String>> groupsByLength = Maps.newHashMap();
+//    for (String s : collection) {
+//        List<String> strings = groupsByLength.get(s.length());
+//        if (strings == null) {
+//            strings = Lists.newArrayList();
+//            groupsByLength.put(s.length(), strings);
+//        }
+//        strings.add(s);
+//    }
+    val groupsByLength = collection. groupBy { s -> s.length()  }
+
+
+//    int maximumSizeOfGroup = 0;
+//    for (List<String> group : groupsByLength.values()) {
+//        if (group.size() > maximumSizeOfGroup) {
+//            maximumSizeOfGroup = group.size();
+//        }
+//    }
+
+    val maximumSizeOfGroup = groupsByLength. values(). map { group -> group.size() }. max()
+
+//    for (List<String> group : groupsByLength.values()) {
+//        if (group.size() == maximumSizeOfGroup) {
+//            return group;
+//        }
+//    }
+
+    return groupsByLength. values(). firstOrNull { group -> group.size() == maximumSizeOfGroup }
+}
+
